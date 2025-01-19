a793cf4f4390a08374fe7694c2d617cbd7e63fa7
Jihoon OH <hoons7412@gmail.com>
2025-01-19 14:17:48 +0900

    Frontend structure organization

--- OURS_homepage/src/App.jsx
+++ OURS_homepage/src/App.jsx
@@ -1,35 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import Sidebar from "./components/ui/Sidebar";
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const title = "총학생회";
+  const data = {
+    "총학생회 소개": "https://www.naver.com/",
+    조직도: "https://www.naver.com/",
+    공약사항: {
+      "시설 부문": "https://www.naver.com/",
+      "교육 부문": "https://www.naver.com/",
+      "복지 부문": "https://www.naver.com/",
+      "소통 부문": "https://www.naver.com/",
+    },
+  };
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Sidebar title={title} data={data} />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
