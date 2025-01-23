3a1da5808e8296e30bf8d96fa5f1aea8c59d0e1d
Jihoon OH <hoons7412@gmail.com>
2025-01-23 21:03:46 +0900

    화면 너비 수정 및 푸터 적용

--- OURS_homepage/src/App.jsx
+++ OURS_homepage/src/App.jsx
@@ -1,5 +1,14 @@
-import "./App.css";
-import Sidebar from "./components/ui/Sidebar";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link,
+  useLocation,
+} from "react-router-dom";
+import Sidebar from "./components/layout/Sidebar";
+import TopBarComponent from "./components/layout/Topbar";
+import MainPage from "./pages/main_page.jsx"; //메인페이지
+import Footer from "./components/layout/Footer.jsx";
 
 function App() {
   const title = "총학생회";
@@ -13,10 +22,46 @@ function App() {
       "소통 부문": "https://www.naver.com/",
     },
   };
+
   return (
-    <>
-      <Sidebar title={title} data={data} />
-    </>
+    <Router>
+      <AppContent data={data} title={title} />
+    </Router>
+  );
+}
+
+function AppContent({ data, title }) {
+  const location = useLocation(); // 현재 경로 추적
+  const hideSidebar = location.pathname === "/";
+  return (
+    <div>
+      <TopBarComponent
+        style={{ position: "fixed", top: 0, left: 0, right: 0, zIndex: 2 }}
+      />
+      {/* 메인 페이지가 아닌 경우에만 사이드바를 렌더링 */}
+      {!hideSidebar && (
+        <Sidebar
+          title={title}
+          data={data}
+          style={{ position: "fixed", top: "60px", left: 0, zIndex: 1 }}
+        />
+      )}
+      <Routes>
+        {/* 기본 경로인 /에 MainPage 컴포넌트 설정 */}
+        <Route path="/" element={<MainPage />} />
+
+        {/* 사이드바가 포함된 다른 페이지들 */}
+        <Route
+          path="/student-council"
+          element={<Link to="https://www.naver.com/">총학생회</Link>}
+        />
+        <Route
+          path="/student-organizations"
+          element={<Link to="https://www.naver.com/">학생자치기구</Link>}
+        />
+      </Routes>
+      <Footer></Footer>
+    </div>
   );
 }
 
