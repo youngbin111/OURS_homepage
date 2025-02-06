aa17c306e0b66aed2c742e6c8e745ddff8fc54a2
Jihoon OH <hoons7412@gmail.com>
2025-02-07 01:44:21 +0900

    라우팅 수정, App.jsx 기본 설정

--- OURS_homepage/src/App.jsx
+++ OURS_homepage/src/App.jsx
@@ -1,60 +1,63 @@
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  useLocation,
-} from "react-router-dom";
-import TopBarComponent from "./components/layout/Topbar";
+import { Routes, Route } from "react-router-dom";
 import MainPage from "./pages/main_page.jsx"; // 메인 페이지
-import Footer from "./components/layout/Footer.jsx";
-import MainContainer from "./components/layout/MainContainer.jsx";
+import NotFound from "./pages/NotFound.jsx";
 import "./styles/global.css";
 
 function App() {
-  const title = "총학생회";
-  const data = {
-    "총학생회 소개": "/student-council",
-    조직도: "/organization-chart",
-    공약사항: "/promises",
-  };
-
   return (
-    <Router>
-      <AppContent data={data} title={title} />
-    </Router>
+    <Routes>
+      <Route path="/" element={<MainPage />} />
+      <Route path="/" element={<MainPage />} />
+      <Route path="/" element={<MainPage />} />
+      <Route path="*" element={<NotFound />} />
+    </Routes>
   );
 }
+// function App() {
+//   const title = "총학생회";
+//   const data = {
+//     "총학생회 소개": "/student-council",
+//     조직도: "/organization-chart",
+//     공약사항: "/promises",
+//   };
 
-function AppContent({ data, title }) {
-  const location = useLocation(); // 현재 경로 추적
-  const hideSidebar = location.pathname === "/";
+//   return (
+//     <Router>
+//       <AppContent data={data} title={title} />
+//     </Router>
+//   );
+// }
 
-  return (
-    <div>
-      <TopBarComponent
-        style={{ position: "fixed", top: 0, left: 0, right: 0, zIndex: 2 }}
-      />
-      {/* 메인 페이지와 다른 경로에서 사이드바 및 컨텐츠 표시 */}
-      {!hideSidebar && (
-        <MainContainer sidebarTitle={title} sidebarData={data} />
-      )}
-      <Routes>
-        {/* 기본 경로 */}
-        <Route path="/" element={<MainPage />} />
-        {/* 학생회 관련 페이지 */}
-        <Route
-          path="/student-council"
-          element={<div>총학생회 소개 페이지입니다.</div>}
-        />
-        <Route
-          path="/organization-chart"
-          element={<div>조직도 페이지입니다.</div>}
-        />
-        <Route path="/promises" element={<div>공약사항 페이지입니다.</div>} />
-      </Routes>
-      <Footer />
-    </div>
-  );
-}
+// function AppContent({ data, title }) {
+//   const location = useLocation(); // 현재 경로 추적
+//   const hideSidebar = location.pathname === "/";
+
+//   return (
+//     <div>
+//       <TopBarComponent
+//         style={{ position: "fixed", top: 0, left: 0, right: 0, zIndex: 2 }}
+//       />
+//       {/* 메인 페이지와 다른 경로에서 사이드바 및 컨텐츠 표시 */}
+//       {!hideSidebar && (
+//         <MainContainer sidebarTitle={title} sidebarData={data} />
+//       )}
+//       <Routes>
+//         {/* 기본 경로 */}
+//         <Route path="/" element={<MainPage />} />
+//         {/* 학생회 관련 페이지 */}
+//         <Route
+//           path="/student-council"
+//           element={<div>총학생회 소개 페이지입니다.</div>}
+//         />
+//         <Route
+//           path="/organization-chart"
+//           element={<div>조직도 페이지입니다.</div>}
+//         />
+//         <Route path="/promises" element={<div>공약사항 페이지입니다.</div>} />
+//       </Routes>
+//       <Footer />
+//     </div>
+//   );
+// }
 
 export default App;
